749fddf3848b8faedd74167d6df0f5f9fd52c415
Callay Jarrett <jcallay@yahoo.com>
2022-12-06 22:31:00 -0500

    working index.php file

--- index.php
+++ index.php
@@ -7,20 +7,20 @@
         <meta name= "viewport" content="width=device-width, initial-scale=1.0"/>
         <link rel="stylesheet"  href="styles.css"/>
         <script src="scripts/login.js"> </script>
+        <script src="scripts/updatecontact.js"> </script>
         <script src="scripts/logout.js"> </script>
         <script src="scripts/homepage.js"> </script>
-        <script src="scripts/newcontact.js"> </script>
+        <script src="scripts/newuser.js"> </script>
         <script src="scripts/newnote.js"> </script>
         <script src="scripts/viewcontact.js"> </script>
+        <script src="scripts/newcontact.js"> </script>
         <script src="scripts/viewusers.js"> </script>
-        <script src="scripts/newuser.js"> </script>
-        <script src="scripts/updatecontact.js"> </script>
         <script src="https://kit.fontawesome.com/6b465b3e69.js" crossorigin="anonymous"></script>
         <title>Dolphin CRM</title>
     </head>
 
     <body>
-        <section id="flex-parent">
+        <section id="parent">
 
             <header>
             <img src="dolphin.png" alt="Logo with a Cute Blue Dolphin"/>
@@ -29,15 +29,15 @@
 
             <div id="central">
 
-                <aside class="sidenav">
-                    <div class="menu"><button id="home"><i class="fas fa-home-lg-alt"></i> Home</button></div>
-                    <div class="menu"><button id ="newcontact"> <i class="fa-solid fa-circle-user"></i> New Contact</button></div>
-                    <div class="menu" id = "adminonly"><button id="viewusers"> <i class="fa-solid fa-user-group"></i>>Users</button></div>
+                <aside class="hide">
+                    <div class="menu"><button id="home"> <img src="images/home.png"/> Home</button></div>
+                    <div class="menu"><button id ="newcontact"> <img src="images/new_contact.png"/> New Contact</button></div>
+                    <div class="menu" id = "admin"><button id="viewusers"> <img src="images/users.png"/>Users</button></div>
                     <hr>
-                    <div class="menu"><button id="logout"> <i class="fa-solid fa-right-from-bracket fa-flip-horizontal"></i>Logout</button></div>
+                    <div class="menu"><button id="logout"> <img src="images/logout.png"/>Logout</button></div>
                 </aside>
 
-                <section id="changearea"> 
+                <section id="change"> 
                     <form id="login">
                         <h1 class="formtitle">Login</h2>
                         <div class="formstatus"> </div>
@@ -47,7 +47,7 @@
                         <div class="formgrp"> 
                             <input class="inputnormal" type="password" placeholder="Password" name="password" required>
                         </div>
-                        <button type= "submit" name="submitbtn" id="submitbtn"> <i class="fa-light fa-lock"></i> <span> Login </span></button>
+                        <button type= "submit" name="submitbtn" id="submitbtn"> <i class="fa-solid fa-lock"></i> <span> Login </span></button>
                     </form> 
                 </section>
 
